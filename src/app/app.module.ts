b7a56c45d3ac84368cf1d7906d82648a5011c856
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-21 01:24:21 +0500

    created serverss component with cli and using nested components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServerssComponent } from './serverss/serverss.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServerssComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/serverss/serverss.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-serverss',
+  templateUrl: './serverss.component.html',
+  styleUrls: ['./serverss.component.css']
+})
+export class ServerssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
