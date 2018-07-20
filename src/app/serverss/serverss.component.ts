1c6c57d2062e0cb5c50bf697df97226cdf8e5727
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-21 01:33:37 +0500

    working with component templates. using template instead of templateUrl

--- src/app/serverss/serverss.component.ts
+++ src/app/serverss/serverss.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-serverss',
-  templateUrl: './serverss.component.html',
+  template: `<app-server></app-server><app-server></app-server>`,
   styleUrls: ['./serverss.component.css']
 })
 export class ServerssComponent implements OnInit {
