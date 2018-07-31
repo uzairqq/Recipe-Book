80df9031669306299c9673ee7a3d42929d6db891
Uzair Iqbal <uzair.qq@outlook.com>
2018-07-31 06:42:36 +0500

    Used NgFor

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -15,6 +15,7 @@ import { Component } from '@angular/core';
 export class ServerComponent {
     serverId = 10;
     serverStatus = 'online';
+    servers = ['test 1', 'test 2 ', 'test 3'];
 
     constructor() {
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
